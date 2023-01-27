10cde2f0da259803e2749cc787632adbea1c4163
Denis <71805486+2D-nz@users.noreply.github.com>
2023-01-27 09:22:59 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
-import Nav from "./components/Nav";
+
 import ToDoContainer from "./components/ToDoContainer";
 
 function App() {
